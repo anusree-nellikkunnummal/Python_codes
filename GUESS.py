c8476ac33c4259a1046134f20f7e0e01fb21c8e8
anusree_narayan <anusreenarayan1@gmail.com>
2022-05-10 20:45:02 +0800

    lines added

--- GUESS.py
+++ GUESS.py
@@ -13,4 +13,20 @@ def guess(x):
             print(f'Sorry, guess again. {guess} is too high')
     print(f'Yay congrats!! You have guessed the number. It is {random_number}')
 
-guess(10)
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low  #could also be high as low = high
+        feedback = input(f'is {guess} too high (H), too low (L), or correct (C) ?? :')
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+    print('Yay !! the computer guessed your number correctly ')
+
+computer_guess(200)
