0710e164049fb8748481c08b1c7bd8b623fa675b
anusree_narayan <anusreenarayan1@gmail.com>
2022-05-10 16:17:58 +0800

    #create a string using user inputs(use string concatenation)

---
+++ main.py
@@ -0,0 +1,27 @@
+#Different Ways to Concatenate String
+#youtuber = 'anusree'
+#print('subscribe to {}'f.format(youtuber)
+#print(f'subscribe to {youtuber}'
+#suppose we want to create a string using user inputs
+adj1 = input('Adjective1:')
+adj2 = input('Adjective2: ')
+adj3 = input('Adjective3: ')
+verb1 = input('Verb1: ')
+verb2 = input('Verb2: ')
+subject1 = input('Subject: ')
+subject2 = input('subject2: ')
+subject3 = input('subject3: ')
+noun1 = input('Noun1: ')
+noun2 = input('Noun2: ')
+noun3 = input('Noun3: ')
+madlib = (f'Life brings us as many {adj1} moments as it does {adj2}, and although there are days we {adj3} there\
+was a manual to {verb1}, it simply wouldn’t be the same without the spontaneity. \
+The journey of life may not become easier as we {verb2} older, but we do seem to understand it better as\
+our perspectives evolve. Whether you’re embarking on a new adventure right out of school or you want \
+to explore different {subject1} in your personal life, it’s never too late to change what the {subject2} looks like. \
+If you’re in need of {subject3} and inspiration, these life quotes from {noun1}, {noun2}, {noun3}\
+, among others, are exactly what you need to hear. Advice from practicing self-love to having\
+a sense of humor to being optimistic never grows old and will surely help you find the push you need to get\
+where you’re going.')
+
+print(madlib)
